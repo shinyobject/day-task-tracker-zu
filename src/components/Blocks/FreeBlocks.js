1b7702f25c355d52223e621eb409d59282645db3
Peter Uchytil <peter@shinyobject.com>
2022-09-11 13:12:05 -0700

    Fixed calculation

--- src/components/Blocks/FreeBlocks.js
+++ src/components/Blocks/FreeBlocks.js
@@ -5,8 +5,14 @@ import { useControls, useBlocks } from "store";
 export const FreeBlocks = styled(({ className }) => {
   const blockSize = useControls((state) => state.blockSize);
   const numberOfHours = useControls((state) => state.numberOfHours);
+  const startHour = useControls((state) => state.startHour);
   const blocks = useBlocks((state) => state.blocks);
-  const blockArray = Object.values(blocks);
+  const blockArray = Object.values(blocks).filter((block) => {
+    const blockTime = block.blockId.match(/.*-(\d*[.]*\d*)/)[1] * 1;
+    console.log(block.blockId, blockTime, blockTime >= startHour);
+    if (blockTime >= startHour) return true;
+    return false;
+  });
   const usedTime = blockArray.reduce(
     (previousValue, currentValue) =>
       currentValue.status !== "free" ? previousValue + 1 : previousValue,
