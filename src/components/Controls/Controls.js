6f5f4c043dbb5565ac6f9fa42357f14fa9aa7eef
Peter Uchytil <peter@shinyobject.com>
2022-12-30 14:18:31 -0800

    Fixed Flexbox

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -2,26 +2,13 @@ import styled from "styled-components";
 import { AddTask } from "./AddTask";
 import { SettingsButton } from "./SettingsButton";
 
-const Left = styled.div`
-  display: flex;
-  gap: 8px;
-`;
-const Right = styled.div`
-  display: flex;
-  gap: 8px;
-`;
-
 export const Controls = styled(({ className, isOpen, setIsOpen }) => {
   return (
     <div className={className}>
-      <Left>
-        <AddTask />
-      </Left>
-      <Right>
-        <SettingsButton onClick={() => setIsOpen(!isOpen)}>
-          Settings
-        </SettingsButton>
-      </Right>
+      <AddTask />
+      <SettingsButton onClick={() => setIsOpen(!isOpen)}>
+        Settings
+      </SettingsButton>
     </div>
   );
 })`
